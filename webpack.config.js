f37587148eaed24ab6adb35abfcbd8ca885a79a4
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-03-10 16:43:26 +0300

    feat: add project folders and files, refactor: file webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const PhpWebpackPlugin = require('php-webpack-plugin');
+
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -20,99 +20,88 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
-		filename: 'index[contenthash].js',
+		filename: '[name][contenthash].js',
 		assetModuleFilename: 'assets/[name][ext]'
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, './src/index.php')
+			template: path.resolve(__dirname, './src/index.html')
 		}),
 		new MiniCssExtractPlugin({
-			filename: '[name].[contenthash].css'
-		}),
-	  new HtmlWebpackPlugin({
-      template: `./test.pug`,
-      filename: `test.php`,
-      foo: 'bar'
-    })
+			filename: '[name][contenthash].css',
+		})
 	],
 	module: {
 		rules: [
-		  {
-			 test: /\.html$/i,
-			 loader: "html-loader",
-		  },
-		  {
-			test: /\.pug$/,
-			use: [
-			  { loader: 'pug-loader' }
-			]
-		 },
-		  {
-			test: /\.(c|sa|sc)ss$/i,
-			use: [
-				devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
-				"css-loader",
-				{
-					loader: 'postcss-loader',
-					options: {
-						postcssOptions: {
-						  plugins: [require('postcss-preset-env')],
-						},
-					 },
-				},
-				"sass-loader"
-			],
-		 }, 
-		 {
-			test: /\.m?js$/,
-			exclude: /node_modules/,
-			use: {
-			  loader: 'babel-loader',
-			  options: {
-				 presets: [
-					['@babel/preset-env', { targets: "defaults" }]
-				 ]
-			  }
-			}
-		 },
-		 {
-			test: /\.(woff|woff2|eot|ttf)/i,
-			type: 'asset/resource',
-			generator: {
-				filename: 'assets/fonts/[name][ext]'
-			}
-		 },
-		 {
-			test: /\.(jpe?g|png|webp|gif|svg)/i,
-			type: 'asset/resource',
-			generator: {
-				filename: 'image/[name][ext]'
+			{
+				test: /\.html$/i,
+				loader: 'html-loader'
 			},
-			use: [
-				{
-					loader: 'image-webpack-loader',
-					options: {
-						mozjpeg: {
-						  progressive: true,
-						},
-						optipng: {
-						  enabled: false,
-						},
-						pngquant: {
-						  quality: [0.65, 0.90],
-						  speed: 4
-						},
-						gifsicle: {
-						  interlaced: false,
-						},
-						webp: {
-						  quality: 75
+			{
+				test: /\.(c|sa|sc)ss$/i,
+				use: [
+					devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
+					"css-loader",
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [require('postcss-preset-env')],
+							}
 						}
-					 }
+					},
+					'sass-loader'
+				],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+				  loader: 'babel-loader',
+				  options: {
+					 presets: [
+						['@babel/preset-env', { targets: "defaults" }]
+					 ]
+				  }
 				}
-			]
-		 }
-		],
-	 },
+			 },
+			 {
+				test: /\.(woff|woff2|eot|ttf)/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'assets/fonts/[name][ext]'
+				}
+			 },
+			 {
+				test: /\.(jpe?g|png|webp|gif|svg)/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'image/[name][ext]'
+				},
+				use: [
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+							  progressive: true,
+							},
+							optipng: {
+							  enabled: false,
+							},
+							pngquant: {
+							  quality: [0.65, 0.90],
+							  speed: 4
+							},
+							gifsicle: {
+							  interlaced: false,
+							},
+							webp: {
+							  quality: 75
+							}
+						 }
+					}
+				]
+			 }
+		]
+	}
 }
